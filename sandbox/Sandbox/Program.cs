bbbf5d1f049fbbce72fa82d5819ee7dcc8ae2fd7
kelechi-jesil-samuel <ksamuel9@byupathway.edu>
2026-08-30 22:18:33 -0700

    done

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,9 @@
-using System;
+﻿    using System;
 
-class Program
-{
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the Sandbox Project.");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! This is the Sandbox");
+        }
     }
-}
